d40e191ea154decbec69c5cfde9532295280b04f
Timur <zamilov05@mail.ru>
2023-09-28 21:55:18 +0300

    Edit 1

---
+++ Z_T_Ramilevich_Project/task_3.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z_T_Ramilevich_Project_1_28.09.2023
+{
+    internal class task_3
+{
+}
+}
